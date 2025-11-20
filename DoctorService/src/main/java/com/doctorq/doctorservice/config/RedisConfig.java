7cdf49d0c7dbd517de19a6c463aff31c0fd1f845
jumapaul <pauljuma736@gmail.com>
2025-11-20 14:43:35 +0300

    minor update on doctor service

--- DoctorService/src/main/java/com/doctorq/doctorservice/config/RedisConfig.java
+++ DoctorService/src/main/java/com/doctorq/doctorservice/config/RedisConfig.java
@@ -9,6 +9,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    //Redis config
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
