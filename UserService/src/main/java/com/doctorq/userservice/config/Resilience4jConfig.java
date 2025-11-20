04382f41eaf389e369963a48251e4b11cf9e740e
jumapaul <pauljuma736@gmail.com>
2025-11-20 14:20:47 +0300

    minor update

--- UserService/src/main/java/com/doctorq/userservice/config/Resilience4jConfig.java
+++ UserService/src/main/java/com/doctorq/userservice/config/Resilience4jConfig.java
@@ -12,6 +12,7 @@ import java.time.Duration;
 @Configuration
 public class Resilience4jConfig {
 
+    //Circuit breaker config
     @Bean
     public CircuitBreakerRegistry circuitBreakerRegistry() {
         CircuitBreakerConfig doctorCircuitBreaker = CircuitBreakerConfig.custom()
