2dbb8d1c3b1322be87ea8f1e18f4326a63c2dc6a
jumapaul <pauljuma736@gmail.com>
2025-12-28 09:39:29 +0300

    minor update

--- AppointmentService/src/main/java/com/doctorq/appointmentservice/appointment/exception/GlobalExceptionHandler.java
+++ AppointmentService/src/main/java/com/doctorq/appointmentservice/appointment/exception/GlobalExceptionHandler.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
 
+/**
+ * Exception handler
+ */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
