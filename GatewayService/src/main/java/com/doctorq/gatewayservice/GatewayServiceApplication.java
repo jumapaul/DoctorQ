8d41bc44c7e9865349fef673899854b42f7fca8b
jumapaul <pauljuma736@gmail.com>
2026-07-24 10:57:07 +0300

    Fixing the helm chart path

--- GatewayService/src/main/java/com/doctorq/gatewayservice/GatewayServiceApplication.java
+++ GatewayService/src/main/java/com/doctorq/gatewayservice/GatewayServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
-
+// Gateway service
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
