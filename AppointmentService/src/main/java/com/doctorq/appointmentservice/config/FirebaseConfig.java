374f18ba641e57ddb95ede2abfe548c50eadb328
jumapaul <pauljuma736@gmail.com>
2026-07-31 17:32:05 +0300

    Minor update

--- AppointmentService/src/main/java/com/doctorq/appointmentservice/config/FirebaseConfig.java
+++ AppointmentService/src/main/java/com/doctorq/appointmentservice/config/FirebaseConfig.java
@@ -4,21 +4,24 @@ package com.doctorq.appointmentservice.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
+    @Value("${app.google.client-secret-path}")
+    private String clientSecretPath;
 
     //Initialize firebase
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
-            InputStream serviceAccount = new ClassPathResource("client_secret.json").getInputStream();
+            InputStream serviceAccount = new FileSystemResource(clientSecretPath).getInputStream();
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
