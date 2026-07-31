30b7ef84bbb916450ac7eef3d0ae19179991fa6c
jumapaul <pauljuma736@gmail.com>
2026-07-31 16:18:20 +0300

    fix: Changing classResource to fileResource

--- UserService/src/main/java/com/doctorq/userservice/config/FirebaseConfig.java
+++ UserService/src/main/java/com/doctorq/userservice/config/FirebaseConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,7 +23,7 @@ public class FirebaseConfig {
     public FirebaseApp firebaseApp() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
 //            InputStream serviceAccount = new ClassPathResource("client_secret.json").getInputStream();
-            InputStream serviceAccount = new ClassPathResource(clientSecretPath).getInputStream();
+            InputStream serviceAccount = new FileSystemResource(clientSecretPath).getInputStream();
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
@@ -37,7 +38,7 @@ public class FirebaseConfig {
 
     @Bean
     public Storage firebaseStorage() throws IOException {
-        InputStream serviceAccount = new ClassPathResource(clientSecretPath).getInputStream();
+        InputStream serviceAccount = new FileSystemResource(clientSecretPath).getInputStream();
 
         GoogleCredentials googleCredentials = GoogleCredentials.fromStream(serviceAccount);
 
